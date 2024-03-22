5b0fe49e55c47ced1a1603a8afee7e00232e1a79
Andres Oe <acojeda.6773@unicesmag.edu.com>
2024-03-22 11:24:03 -0500

    cARGA DE MODELOS

---
+++ app/academics/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+import datetime
+
+#model.DateTimeField(auto_now=True, null=False) => updated_at
+#model.DateTimeField(auto_now_at=True, null=False) => Default now()
+
+# Create your models here.
+class User(models.Model):
+    email = models.EmailField(null=True, blank=True)
+    password = models.CharField(null=True, blank=True, default = True)
+    status = models.CharField(null=True, blank=True, default = True)
+    updated_at = models.DateTimeField(default=datetime.datetime.now ())
+    created_at = models.DateTimeField(default=datetime.datetime.now ())
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    def __str__(self):
+        return self.email
+
+class Person(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    age = models.IntegerField()
+    mobile = models.IntegerField(null=True, blank=True)
+    ident_number = models.CharField(max_length=12, blank=True)
+    updated_at = models.DateTimeField(default=datetime.datetime.now ())
+    created_at = models.DateTimeField(default=datetime.datetime.now ())
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    
+class Cities(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    abrev = models.CharField(max_length=10, blank=True)
+    descrip = models.CharField(max_length=10, blank=True)
+    updated_at = models.DateTimeField(default=datetime.datetime.now ())
+    created_at = models.DateTimeField(default=datetime.datetime.now ())
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    
+class Departments(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    abrev = models.CharField(max_length=10, blank=True)
+    descrip = models.CharField(max_length=10, blank=True)
+    updated_at = models.DateTimeField(default=datetime.datetime.now ())
+    created_at = models.DateTimeField(default=datetime.datetime.now ())
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+class Countries(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    abrev = models.CharField(max_length=10, blank=True)
+    descrip = models.CharField(max_length=10, blank=True)
+    updated_at = models.DateTimeField(default=datetime.datetime.now ())
+    created_at = models.DateTimeField(default=datetime.datetime.now ())
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+class Students(models.Model):
+    code = models.IntegerField()
+    status = models.CharField(null=True, blank=True, default = True)
+    updated_at = models.DateTimeField(default=datetime.datetime.now ())
+    created_at = models.DateTimeField(default=datetime.datetime.now ())
+    deleted_at = models.DateTimeField(null=True, blank=True)
--- app/app_web/wsgi.py
+++ app/app_web/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for app project.
+WSGI config for app_web project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+from decouple import config
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app_web.settings')
 
 application = get_wsgi_application()
